226a2ca924e93424511fc7b3ba2ecac8a6a0d7dd
jobin491 <jobinjosephh2005@gmail.com>
2024-02-29 12:29:36 +0530

    better looking

--- express/routes/admin.js
+++ express/routes/admin.js
@@ -1,25 +1,18 @@
-const express = require('express');
-
 const path = require('path');
 
-const rootDir = require('../util/path')//useless
+const express = require('express');
 
-const productsController = require('../controllers/products');
+const adminController = require('../controllers/admin');
 
 const router = express.Router();
 
-const products = [];
+// /admin/add-product => GET
+router.get('/add-product', adminController.getAddProduct);
 
-router.get('/add-product',productsController.getAddProduct);
+// /admin/products => GET
+router.get('/products', adminController.getProducts);
 
-router.post('/add-product',productsController.postAddProduct);
+// /admin/add-product => POST
+router.post('/add-product', adminController.postAddProduct);
 
-module.exports=router;
-
-
-
-
-
-// router.get('/add-product',(req,res,next)=>{
-//     res.sendFile(path.join(rootDir,'views','add-product.html'));  
-// });
+module.exports = router;
