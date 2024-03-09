9a4aee0de6367ef6ea6790986c78b8e39d55719c
jobin491 <jobinjosephh2005@gmail.com>
2024-03-09 10:31:08 +0530

    removed

--- express/controllers/shop.js
+++ express/controllers/shop.js
@@ -2,33 +2,40 @@ const Product = require('../models/product');
 const Cart = require('../models/cart');
 
 exports.getProducts = (req, res, next) => {
-  Product.fetchAll(products => {
-    res.render('shop/product-list', {
-      prods: products,
-      pageTitle: 'All Products',
-      path: '/products'
+  Product.findAll().then(products=>{
+    res.render('shop/index',{
+      prods:products,
+      pageTitle:'All Products',
+      path:'/products'
     });
+  }).catch(err=>{
+    console.log(err);
   });
 };
 
 exports.getProduct = (req, res, next) => {
   const prodId = req.params.productId;
-  Product.findById(prodId, product => {
+  Product.findById(prodId)
+  .then(([product])=> {
+    console.log(product);
     res.render('shop/product-detail', {
-      product: product,
+      product: product[0],
       pageTitle: product.title,
       path: '/products'
     });
-  });
+  })
+  .catch(err=> console.log(err));
 };
 
 exports.getIndex = (req, res, next) => {
-  Product.fetchAll(products => {
-    res.render('shop/index', {
-      prods: products,
-      pageTitle: 'Shop',
-      path: '/'
+  Product.findAll().then(products=>{
+    res.render('shop/index',{
+      prods:products,
+      pageTitle:'shop',
+      path:'/'
     });
+  }).catch(err=>{
+    console.log(err);
   });
 };
 
