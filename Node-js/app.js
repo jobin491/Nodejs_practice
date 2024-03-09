3e44eaf8b983db6f44655c93ebb4a426eb9075d4
jobin491 <jobinjosephh2005@gmail.com>
2024-03-10 01:25:40 +0530

    CLeared SQL SHOP

--- Node-js/app.js
+++
@@ -1,9 +0,0 @@
-const http= require('http');
-
-const routes = require('./routes');
-
-const server = http.createServer(routes);
-
-server.listen(3000,()=>{
-    console.log("server up and running");
-});   
