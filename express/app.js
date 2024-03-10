c50593540bcf8436686c4a727aa6a23df33459da
jobin491 <jobinjosephh2005@gmail.com>
2024-03-10 20:29:19 +0530

    Done with mongodb

--- express/app.js
+++ express/app.js
@@ -4,8 +4,10 @@ const express = require('express');
 const bodyParser = require('body-parser');
 
 const errorController = require('./controllers/error');
-const mongoConnect = require('./util/database');
+const mongoConnect = require('./util/database').mongoConnect;
+const User = require('./models/user');
 const app = express();
+app.use(express.json());
 
 app.set('view engine', 'ejs');
 app.set('views', 'views');
@@ -15,14 +17,16 @@ const shopRoutes = require('./routes/shop');
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
-
+   
 app.use((req,res,next)=>{
-    // User.findByPk(1)
-    // .then(user=>{
-    //     req.user = user;
-    //     next();
-    // })
-    // .catch(err=> console.log(err));
+    User.findById('65ed87d732dc769988b7500d') 
+    .then(user=>{ 
+        req.user = new User(user.name, user.email, user.cart, user._id);
+        //console.log(user);
+        // console.log(req);
+        next();
+    })
+    .catch(err=> console.log(err));
 });
 
 app.use('/admin', adminRoutes);
--- express/controllers/admin.js
+++ express/controllers/admin.js
@@ -1,4 +1,18 @@
+const mongodb = require('mongodb');
 const Product = require('../models/product');
+
+const ObjectId =mongodb.ObjectId;
+exports.getProducts = (req, res, next) => {
+  Product.fetchAll()
+  .then(products => {
+    res.render('admin/products', {
+      prods: products,
+      pageTitle: 'Admin Products',
+      path: '/admin/products'
+    });
+  })
+  .catch(err=> console.log(err));
+};
 
 exports.getAddProduct = (req, res, next) => {
   res.render('admin/edit-product', {
@@ -13,20 +27,16 @@ exports.postAddProduct = (req, res, next) => {
   const imageUrl = req.body.imageUrl;
   const price = req.body.price;
   const description = req.body.description;
-  req.user
-  .createProduct({
-    title:title,
-    price:price,
-    imageUrl:imageUrl,
-    description:description
-  })
-  .then(result=>{
-    console.log('created');
-    res.redirect('/admin/products')
-  }).catch(err=>{
-    console.log(err);
-  });
-};
+  const product = new Product(title, price, description, imageUrl,null,req.user._id);
+  product
+    .save()
+    .then(result=>{
+      console.log('created');
+      res.redirect('/admin/products')
+    }).catch(err=>{
+      console.log(err);
+    });
+  };
 
 exports.getEditProduct = (req, res, next) => {
   const editMode = req.query.edit;
@@ -34,11 +44,9 @@ exports.getEditProduct = (req, res, next) => {
     return res.redirect('/');
   }
   const prodId = req.params.productId;
-  req.user
-  .getProducts({where:{id:prodId}})
-  // Product.findByPk(prodId)
-  .then(products => {
-    const product = products[0];
+  Product
+  .findById(prodId)
+  .then(product => {
     if (!product) {
       return res.redirect('/');
     }
@@ -58,14 +66,16 @@ exports.postEditProduct = (req, res, next) => {
   const updatedPrice = req.body.price;
   const updatedImageUrl = req.body.imageUrl;
   const updatedDesc = req.body.description;
-  Product.findByPk(prodId)
-  .then(product=>{
-    product.title=updatedTitle;
-    product.price=updatedPrice;
-    product.imageUrl=updatedImageUrl;
-    product.description=updatedDesc;
-    return product.save();
-  })
+  const product =  new Product(
+    updatedTitle,
+    updatedPrice, 
+    updatedDesc, 
+    updatedImageUrl, 
+    prodId
+  );
+  
+  product
+  .save()
   .then(result=>{
     console.log("UPDATED PRODUCT!");
     res.redirect('/admin/products');
@@ -73,26 +83,10 @@ exports.postEditProduct = (req, res, next) => {
   .catch(err=> console.log(err));
 };
 
-exports.getProducts = (req, res, next) => {
-  req.user
-  .getProducts()
-  .then(products => {
-    res.render('admin/products', {
-      prods: products,
-      pageTitle: 'Admin Products',
-      path: '/admin/products'
-    });
-  })
-  .catch(err=> console.log(err));
-};
-
 exports.postDeleteProduct = (req, res, next) => {
   const prodId = req.body.productId;
-  Product.findById(prodId)
-  .then(product=>{
-    return product.destroy();
-  })
-  .then(result=>{
+  Product.deleteById(prodId)
+  .then(()=>{
     console.log("DESTROYED")
     res.redirect('/admin/products');
   })
